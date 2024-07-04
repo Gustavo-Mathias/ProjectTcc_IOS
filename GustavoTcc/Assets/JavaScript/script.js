13a233897ec417fdcd12247e78ce28327350dc34
Gustavo-Mathias <gumathias1304@gmail.com>
2024-07-04 13:18:24 -0300

    trabalhando na responsividade

--- GustavoTcc/Assets/JavaScript/script.js
+++ GustavoTcc/Assets/JavaScript/script.js
@@ -40,10 +40,9 @@ document.addEventListener('DOMContentLoaded', function () {
   checkVisibility();
 });
 
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('nav ul');
-
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('show');
+  });
+  
 
